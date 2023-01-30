0e9c3d035bf06d31f5759810cd49508ec38761b8
Cody Eding <cody.eding@gmail.com>
2023-01-30 11:41:17 -0500

    Install github cli

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,6 +61,20 @@ install_snap_packages() {
     done
 }
 
+#
+# Installs github cli
+#
+install_github_cli() {
+
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+
+}
+
 log_line "Starting bootstrapping"
 
 log_header "Update apt and system"
@@ -68,6 +82,9 @@ sudo apt-get update && sudo apt-get -y dist-upgrade
 
 log_header "Installing apt packages..."
 install_apt_packages
+
+log_header "Installing github cli..."
+install_github_cli
 
 log_header "Installing snap packages..."
 install_snap_packages
